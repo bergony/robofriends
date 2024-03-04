b333575ab47ba0d95130fc5d3c6740e1ac4d78e6
Bergony <bergony.bandeira@quark.tec.br>
2024-03-03 22:57:06 -0300

    Finalizado errorBoundry

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import App from './containers/App';
 import 'tachyons'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greating={'Hello ' + ' React ninja'} />
+      <App/>
   </React.StrictMode>
 );
 
