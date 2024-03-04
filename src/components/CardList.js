922e53cbd61269b28db4ceae1fe0157e67e8a56f
Bergony <bergony.bandeira@quark.tec.br>
2024-03-03 23:08:55 -0300

    Removendo error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,10 +3,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-    if(true){
-        throw new Error('Noo')
-        
-    }
     return (
         <div>
             {
